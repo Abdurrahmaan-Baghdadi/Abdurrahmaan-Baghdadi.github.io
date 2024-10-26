e88ddf5d3517e929317f483851d130d6ad3ee8fa
Abdurrahmaan-Baghdadi <AbdurrahmaanBaghdadi0@gmail.com>
2024-10-26 09:26:36 -0500

    Background Images Set for pages

--- src/assets/Components/Header.tsx
+++ src/assets/Components/Header.tsx
@@ -1,15 +1,23 @@
 import { Link, NavLink } from "react-router-dom"
 
-export default function Header() {
+interface HeaderProps{
+    onActivePageChange: (page: string) => void
+}
+
+export default function Header({ onActivePageChange } : HeaderProps ) {
+    const changeActivePage = (newPage: string) =>
+    {
+        onActivePageChange(newPage)
+    }
     return (
         <header className="site-header">
-            <Link className="site-title" to="/">Abdurrahmaan Baghdadi</Link>
+            <Link onClick={() => changeActivePage('home')} className="site-title" to="/">Abdurrahmaan Baghdadi</Link>
             <nav className="site-pages-list">
-                <NavLink className="site-page-link" to="/">Home</NavLink>
-                <NavLink className="site-page-link" to="projects">Projects</NavLink>
-                <NavLink className="site-page-link" to="aboutMe">About</NavLink>
-                <NavLink className="site-page-link" to="skills">Skills</NavLink>
-                <NavLink className="site-page-link" to="contact">Contact</NavLink>
+                <NavLink onClick={() => changeActivePage('home')} className="site-page-link" to="/">Home</NavLink>
+                <NavLink onClick={() => changeActivePage('projects')} className="site-page-link" to="projects">Projects</NavLink>
+                <NavLink onClick={() => changeActivePage('about-me')} className="site-page-link" to="aboutMe">About</NavLink>
+                <NavLink onClick={() => changeActivePage('skills')} className="site-page-link" to="skills">Skills</NavLink>
+                <NavLink onClick={() => changeActivePage('contact')} className="site-page-link" to="contact">Contact</NavLink>
             </nav>
         </header>
     )
--- src/assets/Components/Layout.tsx
+++ src/assets/Components/Layout.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer"
 
 export default function Layout() {
+    const [activePage, setActivePage] = useState('home')
+
     return (
-        <div className="site-wrapper">
-            <Header />
+        <div className={`site-wrapper ${activePage}-wrapper`}>
+            <Header onActivePageChange={setActivePage}/>
             <main>
                 <Outlet />
             </main>
